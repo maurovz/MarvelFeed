a27b16efcdde6f15f090c2b6cdcf6e5ce6949785
Mauricio Vazquez <Mauricio.Vazquez@navigaglobal.com>
2021-10-16 14:10:11 +0200

    Add character mapper valid test

--- FeedFeatureTests/CharacterMapperTest.swift
+++ FeedFeatureTests/CharacterMapperTest.swift
@@ -7,4 +7,17 @@ class CharacterMapperTest: XCTestCase {
 
     XCTAssertThrowsError(try CharacterMapper.map(data: invalidJSON))
   }
+
+  func test_map_deliversCharacterWithValidJSON() throws {
+    let character = makeCharacter(name: "Hero Tester",
+                             description: "A testing hero",
+                             thumbnail: URL(string: "http://any-url.com")!,
+                             comicName: "Testing Hero Comic 1",
+                             comicDescription: "Testing Hero Chapter 1",
+                             comicThumbnail: URL(string: "http://any-url.com")!)
+
+    let result = try CharacterMapper.map(data: makeJSON(character.json))
+
+    XCTAssertEqual(result, character.model)
+  }
 }
