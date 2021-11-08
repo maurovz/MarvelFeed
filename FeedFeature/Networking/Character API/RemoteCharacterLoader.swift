c822eec555b3d86763cad6e0c8da5679ebb67b98
Mauricio Vazquez <maurovz@gmail.com>
2021-11-08 20:14:49 +0100

    Create CoreData Stack and character cache (#13)

--- FeedFeature/Networking/Character API/RemoteCharacterLoader.swift
+++ FeedFeature/Networking/Character API/RemoteCharacterLoader.swift
@@ -3,10 +3,12 @@ import Foundation
 public final class RemoteCharacterLoader: CharacterLoader {
   private let url: URL
   private let client: HTTPClient
+  private let cache: CoreDataServices
 
-  public init(url: URL, client: HTTPClient) {
+  public init(url: URL, client: HTTPClient, cache: CoreDataServices) {
     self.url = url
     self.client = client
+    self.cache = cache
   }
 
   public typealias Result = CharacterLoader.Result
@@ -20,11 +22,23 @@ public final class RemoteCharacterLoader: CharacterLoader {
           return
         }
 
+        self.setCache(characters: mappedCharacter)
         completion(.success(mappedCharacter))
 
       case .failure(let error):
-        completion(.failure(error))
+        let cacheCharacters = self.cache.loadCharactersFromCoreData()
+        guard !cacheCharacters.isEmpty else {
+          completion(.failure(error))
+          return
+        }
+
+        completion(.success(cacheCharacters))
       }
     }
+  }
+
+  private func setCache(characters: [Character]) {
+    guard cache.deleteEntityFromCoreData(entity: "CharacterDescription") else { return }
+    cache.saveCharactersToCache(characters: characters)
   }
 }
